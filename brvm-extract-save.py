27c6ac5a6e1e1b84d650a9f0d64c786fc82fd99a
SoroPierre <56124430+SoroPierre@users.noreply.github.com>
2025-10-08 16:18:10 +0200

    Add BRVM extraction and save script

---
+++ brvm-extract-save.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+from os import path, makedirs
+
+# URL de la page BRVM
+url = "https://www.brvm.org/fr/cours-actions/0"
+# Use runner workspace for storage in GitHub Actions
+path_folder_storage = "Historiques"
+
+''' Permet de récupérer les cours d'ouverture et clôture de brvm '''
+
+response = requests.get(url, verify=False)
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table')
+table_cotation = tables[3]
+rows = table_cotation.find_all('tr')
+
+cotation_data = []
+
+for row in rows[1:]:
+    columns = row.find_all('td')
+    if len(columns) > 1:
+        ticker = columns[0].text.strip()
+        nom_valeur = columns[1].text.strip()
+        volume_transaction = columns[2].text.strip()
+        open_cours = columns[4].text.strip()
+        close_cours = columns[5].text.strip()
+        variation = columns[6].text.strip()
+        cotation_data.append((ticker, nom_valeur, volume_transaction, open_cours, close_cours, variation))
+
+# Sauvegarder dans un fichier CSV
+if not path.exists(path_folder_storage):
+    makedirs(path_folder_storage)
+
+filename = f"brvm_cours_{datetime.now().strftime('%Y%m%d')}.csv"
+filename = path.join(path_folder_storage, filename)
+
+with open(filename, "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Symbole", "Nom", "Volume", "Cours ouverture", "Cours Cloture", "Variation"])
+    writer.writerows(cotation_data)
